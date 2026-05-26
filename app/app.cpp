6cd729d8886b67a6e788737dbe8fd8a9809a07d4
ryan3-10 <ryan.coutts98@gmail.com>
2026-05-26 13:00:22 -0600

    implement simple main loop in app.cpp

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,16 @@
 #include "app.h"
+#include "sht31_sensor.h"
+#include "weather_data.h"
 #include <stdio.h>
 #include <stm32f4xx_hal.h>
 
 void run_app(I2C_HandleTypeDef* hi2c) {
-
+	Sht31Sensor sensor(hi2c);
+	WeatherData wd(sensor);
 
 	while (true) {
-
+		printf("Temp: %i Hum: %i\n", (int)wd.getTemperature(), (int)wd.getHumidity());
+		wd.update();
 	}
 }
 
