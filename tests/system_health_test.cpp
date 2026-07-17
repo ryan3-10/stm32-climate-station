535c7205dfb2d93b97208d33aea2c4a020bfc8b3
ryan3-10 <ryan.coutts98@gmail.com>
2026-07-17 11:56:50 -0600

    Write tests for system_health

---
+++ tests/system_health_test.cpp
@@ -0,0 +1,72 @@
+#include "health_checkable.h"
+#include "system_health.h"
+#include <gtest/gtest.h>
+#include <string>
+
+class HealthCheckablemock : public IHealthCheckable {
+public:
+    HealthCheckablemock(const std::string& err, bool startingOkFlag)
+        : errorCode(err)
+        , okFlag(startingOkFlag)
+    {}
+
+    const char* getErrorCode() const override {
+        return errorCode.data();
+    }
+
+    bool isOk() const override {
+        return okFlag;
+    }
+
+    void runHealthCheck() override {
+        hasBeenChecked = true;
+    }
+
+    std::string errorCode;
+    bool okFlag;
+    bool hasBeenChecked = false;
+};
+
+TEST(SystemHealthTest, OnlyHealthChecksErrorsOnUpdate) {
+    HealthCheckablemock mockStatusOk{"", true};
+    HealthCheckablemock mockStatusErr{"", false};
+    SystemHealth sh{{&mockStatusOk, &mockStatusErr, nullptr}};
+    
+    // Health check error component
+    sh.update();
+
+    EXPECT_TRUE(mockStatusErr.hasBeenChecked);
+    EXPECT_FALSE(mockStatusOk.hasBeenChecked);
+}
+
+TEST(SystemHealthTest, ProducesSysemOkSummaryWhenNoErrors) {
+    HealthCheckablemock mock1{"", true};
+    HealthCheckablemock mock2{"", true};
+    HealthCheckablemock mock3{"", true};
+    SystemHealth sh{{&mock1, &mock2, &mock3}};
+
+    std::string summary = sh.getHealthSummary().data();
+    EXPECT_EQ(summary, "System Ok");
+}
+
+TEST (SystemHealthTest, ProducesErrorSummaryWhenErrorsPresent) {
+    HealthCheckablemock mock1{"m1", false};
+    HealthCheckablemock mock2{"m2", true};
+    HealthCheckablemock mock3{"m3", true};
+    SystemHealth sh{{&mock1, &mock2, &mock3}};
+
+    // With just 1 failed component
+    std::string summary = sh.getHealthSummary().data();
+    EXPECT_EQ(summary, "Err: m1");
+
+    // With 2 failed components
+    mock3.okFlag = false;
+    summary = sh.getHealthSummary().data();
+    EXPECT_EQ(summary, "Err: m1 m3");
+
+    // With 3 failed components
+    mock2.okFlag = false;
+    summary = sh.getHealthSummary().data();
+    EXPECT_EQ(summary, "Err: m1 m2 m3");
+}
+
